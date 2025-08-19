1c86157e2bf91c9e0ef1feba4fc393e8b45cce4c
codeiina <raihananisrina04@gmail.com>
2025-08-19 09:49:35 +0700

    Create heavy.js

---
+++ api/heavy.js
@@ -0,0 +1,8 @@
+export default function handler(req, res) {
+  let sum = 0;
+  for (let i = 0; i < 1e7; i++) sum += i;
+  res.status(200).json({
+    sum,
+    time: new Date().toISOString(),
+  });
+}
